86e7023ad96f0ec04e5fa1a6353df974640fd891
Wesley <wesleycpdev@gmail.com>
2023-04-20 10:42:03 -0300

    Revert "Commit"
    This reverts commit 89475d8f901a4ce2c2122f8f83d7f53e9c30484f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LiturgiaService } from 'src/services/liturgia.service';
-import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LiturgiaService, SocialSharing],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LiturgiaService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { LiturgiaService } from 'src/services/liturgia.service';
-import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -11,27 +10,14 @@ export class HomePage {
 
   liturgiaDoDia: any;
 
-  constructor(private liturgiaService: LiturgiaService, private socialSharing: SocialSharing) {
+  constructor(liturgiaService: LiturgiaService) {
     setTimeout(() => {
-      this.liturgiaService.liturgiaDoDia()
+      liturgiaService.liturgiaDoDia()
     .subscribe(data => {
       this.liturgiaDoDia = data;
       this.liturgiaDoDia.evangelho.titulo = this.liturgiaDoDia.evangelho.titulo.replace('✠', '✠ ')
     })
     }, 500);
-  }
-
-  share() {
-    const message = `Confira a liturgia diária de hoje! ${this.liturgiaDoDia?.liturgia}`;
-    const subject = 'Liturgia Diária';
-    const file = undefined;
-    const url = 'https://liturgia-diaria.vercel.app/';
-
-    this.socialSharing.share(message, subject, file, url).then(() => {
-      console.log('Compartilhado com sucesso!');
-    }).catch(() => {
-      console.log('Erro ao compartilhar.');
-    });
   }
 
 }

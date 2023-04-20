89475d8f901a4ce2c2122f8f83d7f53e9c30484f
Wesley <wesleycpdev@gmail.com>
2023-04-20 10:31:28 -0300

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LiturgiaService } from 'src/services/liturgia.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LiturgiaService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LiturgiaService, SocialSharing],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

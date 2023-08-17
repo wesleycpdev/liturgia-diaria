18fc94c80c134007eb9f8627d7e7db260d5f00fc
Wesley <wesleycpdev@gmail.com>
2023-08-17 12:47:00 -0300

    Commit

--- src/app/liturgia/liturgia.page.ts
+++ src/app/liturgia/liturgia.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { LiturgiaService } from 'src/services/liturgia.service';
 
 @Component({
@@ -12,35 +11,19 @@ export class LiturgiaPage {
   liturgiaDoDia: any;
 
   constructor(
-    public liturgiaService: LiturgiaService,
-    public sanitizer: DomSanitizer
+    public liturgiaService: LiturgiaService
   ) {
     setTimeout(() => {
       liturgiaService.liturgiaDoDia()
-    .subscribe(data => {
-      this.liturgiaDoDia = data;
-      this.liturgiaDoDia.evangelho.titulo = this.liturgiaDoDia.evangelho.titulo.replace('✠', '✠ ')
-    })
+      .subscribe(data => {
+        this.liturgiaDoDia = data;
+        this.liturgiaDoDia.evangelho.titulo = this.liturgiaDoDia.evangelho.titulo.replace('✠', '✠ ')
+      })
     }, 500);
   }
 
-  get textoPrimeiraLeitura () {
-    let textoProcessado = this.liturgiaDoDia.primeiraLeitura.texto.replace(/(\d+)(\D)/g, '\n\n<span style="color: red;">$1</span> $2')
-    return this.sanitizer.bypassSecurityTrustHtml(textoProcessado)
-  }
-  
   get textoSalmo() {
     return this.liturgiaDoDia.salmo.texto.replace(/—/g, ' \n')
-  }
-  
-  get textoSegundaLeitura () {
-    let textoProcessado = this.liturgiaDoDia.segundaLeitura.texto.replace(/(\d+)(\D)/g, '\n\n<span style="color: red;">$1</span> $2')
-    return this.sanitizer.bypassSecurityTrustHtml(textoProcessado)
-  }
-
-  get textoEvangelho() {
-    let textoProcessado = this.liturgiaDoDia.evangelho.texto.replace(/(\d+)(\D)/g, '\n\n<span style="color: red;">$1</span> $2')
-    return this.sanitizer.bypassSecurityTrustHtml(textoProcessado)
   }
 
 }

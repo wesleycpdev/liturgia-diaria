d9040f224a2d8d4effacbdb8696eafcf2218929d
Wesley <wesleycpdev@gmail.com>
2024-11-28 17:34:51 -0300

    commit

--- src/app/liturgia/liturgia.page.ts
+++ src/app/liturgia/liturgia.page.ts
@@ -9,6 +9,8 @@ import { LiturgiaService } from 'src/services/liturgia.service';
 export class LiturgiaPage {
 
   liturgiaDoDia: any;
+  numbers = { '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', 
+    '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹' };
 
   constructor(
     public liturgiaService: LiturgiaService
@@ -16,8 +18,33 @@ export class LiturgiaPage {
     setTimeout(() => {
       liturgiaService.liturgiaDoDia()
       .subscribe(data => {
-        this.liturgiaDoDia = data;
+        this.liturgiaDoDia = data
         this.liturgiaDoDia.evangelho.titulo = this.liturgiaDoDia.evangelho.titulo.replace('✠', '✠ ')
+
+        const numbers = {
+          '0': '⁰',
+          '1': '¹',
+          '2': '²',
+          '3': '³',
+          '4': '⁴',
+          '5': '⁵',
+          '6': '⁶',
+          '7': '⁷',
+          '8': '⁸',
+          '9': '⁹'
+        };
+
+        this.liturgiaDoDia.primeiraLeitura.texto = this.liturgiaDoDia.primeiraLeitura.texto.replace(/(\d{1,3})(?=\s?[^\d\s,.;!?])/g, (match) => {
+          return match.split('').map(char => numbers[char]).join('');
+        });
+
+        this.liturgiaDoDia.segundaLeitura.texto = this.liturgiaDoDia.primeiraLeitura.texto.replace(/(\d{1,3})(?=\s?[^\d\s,.;!?])/g, (match) => {
+          return match.split('').map(char => numbers[char]).join('');
+        });
+
+        this.liturgiaDoDia.evangelho.texto = this.liturgiaDoDia.evangelho.texto.replace(/(\d{1,3})(?=\s?[^\d\s,.;!?])/g, (match) => {
+          return match.split('').map(char => numbers[char]).join('');
+        });
       })
     }, 500);
   }
